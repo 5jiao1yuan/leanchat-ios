3123a7f6a85865aa04e739fad9a10468364c45d3
都基鹏 <dylandu123@163.com>
2016-06-30 11:29:48 +0800

    增加红包3.0阿里回调

--- LeanChat/LeanChat/CDAppDelegate.m
+++ LeanChat/LeanChat/CDAppDelegate.m
@@ -222,5 +222,47 @@
     [OpenShare handleOpenURL:url];
     return YES;
 }
+#ifdef REDPACKET_AVALABLE
+
+#pragma mark - Alipay
+
+/**
+ *  iOS9 statusBar上的快捷返回监听
+ */
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    [[NSNotificationCenter defaultCenter] postNotificationName:RedpacketAlipayNotifaction object:nil];
+}
+
+// NOTE: 9.0之前使用的API接口
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    
+    if ([url.host isEqualToString:@"safepay"]) {
+        //跳转支付宝钱包进行支付，处理支付结果
+        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            [[NSNotificationCenter defaultCenter] postNotificationName:RedpacketAlipayNotifaction object:resultDic];
+        }];
+    }
+    return YES;
+}
+
+// NOTE: 9.0以后使用新API接口
+- (BOOL)application:(UIApplication *)app
+            openURL:(NSURL *)url
+            options:(NSDictionary<NSString*, id> *)options
+{
+    if ([url.host isEqualToString:@"safepay"]) {
+        //跳转支付宝钱包进行支付，处理支付结果
+        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            [[NSNotificationCenter defaultCenter] postNotificationName:RedpacketAlipayNotifaction object:resultDic];
+        }];
+    }
+    return YES;
+}
+
+#endif
 
 @end
